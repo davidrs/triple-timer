868648ba8b50ff82c962f5dac80e59f730762d62
David Rust-Smith <david@wearethorn.org>
2023-04-20 18:04:32 -0700

    add timer features

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # triple-timer
 App for timing.
+
+http://localhost:8000 192.168.2.25:8000
+
+run html version
+cd website  && python -m http.server
+
--- website/script.js
+++ website/script.js
@@ -2,67 +2,83 @@
 let actor_movies = {};
 let current_name='';
 let seed=0;
-let get_actors = credits => {
-    let actor_names = []
-    let actor_movies = {}
-    for (var c of credits){
-        let credit = JSON.parse(c['cast'])
-        for (p of credit){
-            actor_names.push(p['name'])
-            if (!actor_movies.hasOwnProperty(p['name'])){
-                actor_movies[p['name']]=[]
-            }
-            actor_movies[p['name']].push(c['title'])
-        }
+let start_time=0;
+let previous_times=[];
 
+
+// Javascript that sets time id to 00:00 when someone presses restart
+$( "#restart" ).click(function() {
+    $("#time").text('00:00');
+    // Set start_time to now.
+    start_time = Date.now();
+});
+
+
+
+function render_times() {    
+
+    // This code displays the date and time of the previous times that the user 
+    // clicked the button in an html table.
+    // The table has three columns: <th>Start Time</th>, <th>15 min end</th>, <th>2hr end</th>
+    // The table is sorted by the start time.
+    // The table is cleared and re-rendered every time the user clicks the button.
+    
+    // Clear the table.
+    $("#history").html('');
+    // Sort the previous_times list.
+    previous_times.sort();
+    // Add the table header.
+    $("#history").append('<tr><th>Start Time</th><th>15 min end</th><th>2hr end</th></tr>');
+    // Add the table rows.
+    for (let i = 0; i < previous_times.length; i++) {
+        let date = new Date(previous_times[i]);
+        let date_string = date.toLocaleDateString();    
+        let time_string = date.toLocaleTimeString();
+        let fifteen_min_end = new Date(previous_times[i] + 15*60000);
+        // Only show hours and minutes.
+        let fifteen_min_end_string = fifteen_min_end.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+        let two_hr_end = new Date(previous_times[i] + 120*60000);
+        let two_hr_end_string = two_hr_end.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+        $("#history").append('<tr><td>' + date_string + ' ' + time_string + '</td><td>' + fifteen_min_end_string + '</td><td>' + two_hr_end_string + '</td></tr>');
     }
-    return [actor_names, actor_movies]
-};
 
-let shuffleWord = (word) => {
-    const shuffled= word.toLowerCase().split('').sort().join('')
-    return shuffled
-};
-
-$( "#answer" ).click(function() {
-    $("#name").text(current_name);
-});
-
-$( "#hint" ).click(function() {
-    $("#movies").append('\n<br/>' + actor_movies[current_name][Date.now()%actor_movies[current_name].length]);
-});
-
-$( "#next" ).click(function() {  
-    seed++;
-    current_name = randomActor(actor_names)
-    playRound(current_name,actor_movies,false)
-});
-
-let playRound = (name, actor_movies) =>{
-    let shfl = shuffleWord(name)
-    $("#name").text(shfl);
-    $("#movies").text(actor_movies[name][Date.now()%actor_movies[name].length]);
-};
-
-let randomActor = (actor_names) =>{
-    return actor_names[Math.round(seed+Date.now()/(60*200))%actor_names.length]
 }
 
-let playGame = actor_movies =>{
-    console.log('actor_names loaded',actor_movies)
-    // let [actor_names, actor_movies] = get_actors(data)
-    actor_names = Object.keys(actor_movies)
-    console.log('actors loaded',actor_names)
-    current_name = randomActor(actor_names)
-    playRound(current_name,actor_movies,false)
-};
 
-let loadData= () =>{
-    $.getJSON( "./actor_movies_200.json", function( data ) {
-        actor_movies = data;
-        playGame(actor_movies);
-        
-      });
-};
+$( "#start" ).click(function() {
+    // Set start_time to now.
+    start_time = Date.now();
+    // Add start_time list o previous_times
+    previous_times.push(start_time);
+    render_times();
 
-loadData();
+    // Set time to 00:00
+    $("#time").text('00:00');
+
+    // Set the timer to update every 1000ms.
+    setInterval(function() {
+        // Get the current time.
+        var now = Date.now();
+        // Calculate the difference between now and start_time.
+        var diff = now - start_time;
+        // Calculate the minutes and seconds.
+        var minutes = Math.floor(diff / 60000);
+        var seconds = Math.floor((diff % 60000) / 1000);
+        // Update the timer.
+        $("#time").text(minutes + ":" + seconds);
+    }, 1000);
+
+    // Save list of previous times to local storage.
+    localStorage.setItem('previous_times', JSON.stringify(previous_times));
+
+});
+
+
+// Once page loads, load previous_times from local storage.
+$(document).ready(function() {
+    previous_times = JSON.parse(localStorage.getItem('previous_times'));
+    if (previous_times == null) {
+        previous_times = [];
+    }
+    render_times();
+});
