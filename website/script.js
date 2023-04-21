82227e642ae1289006449bf5f2ce37d5e61bfd6b
David Rust-Smith <david@wearethorn.org>
2023-04-20 18:50:53 -0700

    cleanup

--- website/script.js
+++ website/script.js
@@ -6,14 +6,6 @@ let start_time=0;
 let previous_times=[];
 
 
-// Javascript that sets time id to 00:00 when someone presses restart
-$( "#restart" ).click(function() {
-    $("#time").text('00:00');
-    // Set start_time to now.
-    start_time = Date.now();
-});
-
-
 
 function render_times() {    
 
@@ -25,8 +17,8 @@ function render_times() {
     
     // Clear the table.
     $("#history").html('');
-    // Sort the previous_times list.
-    previous_times.sort();
+    // Reverse Sort the previous_times list.
+    previous_times.sort(function(a, b){return b-a});
     // Add the table header.
     $("#history").append('<tr><th>Start Time</th><th>15 min end</th><th>2hr end</th></tr>');
     // Add the table rows.
@@ -39,21 +31,59 @@ function render_times() {
         let fifteen_min_end_string = fifteen_min_end.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
         let two_hr_end = new Date(previous_times[i] + 120*60000);
         let two_hr_end_string = two_hr_end.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+        // Only show date if first of that day.
+        if (i > 0) {
+            let previous_date = new Date(previous_times[i-1]);
+            if (date.getDate() == previous_date.getDate()) {
+                date_string = '';
+            }
+        }   
+
         $("#history").append('<tr><td>' + date_string + ' ' + time_string + '</td><td>' + fifteen_min_end_string + '</td><td>' + two_hr_end_string + '</td></tr>');
     }
 
 }
 
+// #delete button should clear previous_times list and local storage.
+$( "#delete" ).click(function() {
+    previous_times = [];
+    localStorage.setItem('previous_times', JSON.stringify(previous_times));
+    render_times();
+});
+
+
 
 $( "#start" ).click(function() {
     // Set start_time to now.
     start_time = Date.now();
+    localStorage.setItem('start_time', start_time);
+
     // Add start_time list o previous_times
     previous_times.push(start_time);
     render_times();
 
-    // Set time to 00:00
-    $("#time").text('00:00');
+
+    // Save list of previous times to local storage.
+    localStorage.setItem('previous_times', JSON.stringify(previous_times));
+
+});
+
+
+// Once page loads, load previous_times from local storage.
+$(document).ready(function() {
+    //load start time from local storage
+    start_time = localStorage.getItem('start_time');
+    if (start_time == null) {
+        start_time = 0;
+    }
+
+
+    //load previous times from local storage    
+    previous_times = JSON.parse(localStorage.getItem('previous_times'));
+    if (previous_times == null) {
+        previous_times = [];
+    }
+    render_times();
 
     // Set the timer to update every 1000ms.
     setInterval(function() {
@@ -64,21 +94,7 @@ $( "#start" ).click(function() {
         // Calculate the minutes and seconds.
         var minutes = Math.floor(diff / 60000);
         var seconds = Math.floor((diff % 60000) / 1000);
-        // Update the timer.
-        $("#time").text(minutes + ":" + seconds);
+        // Update the #time, pad minutes and seconds with 0 if needed.
+        $('#time').html(minutes.toString().padStart(2, '0') + ':' + seconds.toString().padStart(2, '0'));
     }, 1000);
-
-    // Save list of previous times to local storage.
-    localStorage.setItem('previous_times', JSON.stringify(previous_times));
-
-});
-
-
-// Once page loads, load previous_times from local storage.
-$(document).ready(function() {
-    previous_times = JSON.parse(localStorage.getItem('previous_times'));
-    if (previous_times == null) {
-        previous_times = [];
-    }
-    render_times();
 });
